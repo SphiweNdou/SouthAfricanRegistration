c3a23b2081bfaad3fcc1c52d1329c9a7cbab9a02
Sphiwe Ndou <ndebondou@gmail.com>
2025-02-20 19:52:28 +0200

    Added calculateUserAge method Refractored user model for encapsulation

--- SouthAfricanRegistration.Server/Models/User.cs
+++ SouthAfricanRegistration.Server/Models/User.cs
@@ -1,10 +1,36 @@
 ﻿namespace SouthAfricanRegistration.Server.Models
 {
-    public class User
+    internal class User
     {
-        public int Id { get; set; }
-        public int SouthAfricanID { get; set; }
+        public int Id { get; private set; }
+        public string SouthAfricanIdNumber { get; private set; }
         public string Name { get; set; }
-        public int Age { get; set; }
+        public int Age { get; private set; }
+
+        public User(string name, string IdNumber) 
+        {
+            Name = name;
+            SouthAfricanIdNumber = IdNumber;
+            Age = CalculateUserAge(IdNumber);
+        }
+
+        private static int CalculateUserAge(string idNumber)
+        {
+            if( idNumber.Length != 13 || !int.TryParse(idNumber, out _) )
+                return 0;
+            
+            string birthYear = idNumber[..2];
+            string birthMonth = idNumber[2..4];
+            string birthDay = idNumber[4..6];
+
+            int year = int.Parse(birthYear) > DateTime.Now.Year - 2000 ? 1900 + int.Parse(birthYear) : 2000 + int.Parse(birthYear);
+            DateTime birthDate = new DateTime(year, int.Parse(birthMonth), int.Parse(birthDay));
+            int age = DateTime.Today.Year - birthDate.Year;
+            
+            if (birthDate > DateTime.Today.AddYears(-age)) 
+                age--;
+            
+            return age;
+        }
     }
 }
