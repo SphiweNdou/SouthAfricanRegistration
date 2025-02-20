4ac2a684ec7fa090d63c27f44630d54f7735dfbf
Sphiwe Ndou <ndebondou@gmail.com>
2025-02-20 19:59:45 +0200

    Add Decoupled User repository

---
+++ SouthAfricanRegistration.Server/Data/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using SouthAfricanRegistration.Server.Models;
+
+namespace SouthAfricanRegistration.Server.Data
+{
+    internal interface IUserRepository
+    {
+        Task<List<User>> GetUsersAsync();
+        Task AddUserAsync(User user);
+    }
+}
