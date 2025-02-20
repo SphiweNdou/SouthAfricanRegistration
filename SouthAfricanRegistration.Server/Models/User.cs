f4ed58c1c432ae9dd32b82bd2ff3b46c96d5ad68
Sphiwe Ndou <ndebondou@gmail.com>
2025-02-20 18:58:55 +0200

    Add user Model and user Dto

---
+++ SouthAfricanRegistration.Server/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace SouthAfricanRegistration.Server.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int SouthAfricanID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ SouthAfricanRegistration/Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SouthAfricanRegistration.Client.Models
+{
+    public class UserDto
+    {
+        public string Name { get; set; }
+        public int SouthAfricanID { get; set; }
+    }
+}
