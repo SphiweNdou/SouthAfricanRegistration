90b642e7ab6f5b0c025d13826cd0e070725fc549
Sphiwe Ndou <ndebondou@gmail.com>
2025-02-20 19:59:20 +0200

    Add DB context EF

---
+++ SouthAfricanRegistration.Server/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SouthAfricanRegistration.Server.Models;
+
+namespace SouthAfricanRegistration.Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
