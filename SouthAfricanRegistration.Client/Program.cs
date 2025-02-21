119e178362381104f1cf298b8380ba5e3b51f787
Sphiwe Ndou <sphiwe.ndou@siemens.com>
2025-02-21 10:07:39 +0200

    Addded appSettings to client application

--- SouthAfricanRegistration.Client/Program.cs
+++ SouthAfricanRegistration.Client/Program.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using SouthAfricanRegistration.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7282/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetValue<string>("Api-endpoint")) });
 
 await builder.Build().RunAsync();
