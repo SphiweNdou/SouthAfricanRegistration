419c3e4e393aed73a8b59aa2e9c3ea3752f261ac
Sphiwe Ndou <sphiwe.ndou@siemens.com>
2025-02-21 15:57:35 +0200

    Code cleanup

--- SouthAfricanRegistration.Client/Program.cs
+++ SouthAfricanRegistration.Client/Program.cs
@@ -1,12 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using SouthAfricanRegistration.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetValue<string>("Api-endpoint")) });
 
 await builder.Build().RunAsync();
--- SouthAfricanRegistration.Server/Program.cs
+++ SouthAfricanRegistration.Server/Program.cs
@@ -3,36 +3,27 @@ using SouthAfricanRegistration.Server.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
-
 var builder = WebApplication.CreateBuilder(args);
-
-// Enable Controllers
 builder.Services.AddControllers();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options => 
 {
     options.SwaggerDoc("v1", new OpenApiInfo
     {
-        Title = "Personal Finace Tracker API",
+        Title = "South African Registration",
         Version = "v1"
     });
  });
-// Register Services
 
-//DB configuration
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
-
-
-//allow cross-origin resource sharing domians, httpMethods, and headers jwt etc..
 builder.Services.AddCors(policy => policy.AddPolicy("AllowAll", opt => opt.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 
 var app = builder.Build();
@@ -40,7 +31,7 @@ app.UseCors("AllowAll");
 app.UseRouting();
 app.UseAuthorization();
 app.MapControllers();
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
